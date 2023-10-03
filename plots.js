2d48261b60818cb64fd6fb1bba1268719b2ab589
TapleenLamba <tapleen.lamba@gmail.com>
2023-10-03 10:45:59 -0400

    html and js changes
    html and js changes

--- plots.js
+++ plots.js
@@ -1,17 +1,41 @@
-let trace1 = {
-    x: xData, // make this the dates
-    y: yData // make this the values
+d3.csv('merged.csv').then(function(data) {
+  // Convert date strings to Date objects
+  data.forEach(function(d) {
+      d.Date = new Date(d.Date);
+      d.Price = parseFloat(d.Price);
+  });
 
-    // x: data.map(row => row.greekName),
-    // y: data.map(row => row.greekSearchResults)
-    // greekName and greekSearchResults are column names from the .js file we need to produce
+  // Extract date and price data for plotting
+  const xData = data.map(d => d.Date);
+  const yData = data.map(d => d.Price);
+
+  // Create a Plotly trace for the stock price
+  let trace1 = {
+      x: xData,
+      y: yData,
+      type: 'scatter',
+      mode: 'lines',
+      name: 'Stock Price'
   };
-  
-  let data = [trace1];
-  
+
+  // Define the data array for the plot
+  let plotData = [trace1];
+
+  // Define the layout for the plot
   let layout = {
-    title: "Stock Value per Day"
+      title: 'Stock Value per Day',
+      xaxis: {
+          title: 'Date'
+      },
+      yaxis: {
+          title: 'Price'
+      }
   };
+
+  // Create the Plotly chart
+  Plotly.newPlot('plot', plotData, layout);
+});
+
+
   
-  Plotly.newPlot("plot", data, layout);
 
